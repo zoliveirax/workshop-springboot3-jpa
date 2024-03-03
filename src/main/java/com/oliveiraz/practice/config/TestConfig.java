3c3ce4fc64ef9bd36bf56980a0300bd11d5926b7
Julio Oliveira <juliomatheusao@gmail.com>
2024-03-02 22:40:21 -0300

    ManyToMany association with JoinTable

--- src/main/java/com/oliveiraz/practice/config/TestConfig.java
+++ src/main/java/com/oliveiraz/practice/config/TestConfig.java
@@ -52,6 +52,18 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		
+		// Associação de objetos no Paradigma Orientado a Objetos //
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+
 		User u1 = new User(null, "Alex Silva", "alex@gmail.com", "983348888", "123456");
 		User u2 = new User(null, "Julio Oliveira", "julio@gmail.com", "977132777", "123456"); 
 		
